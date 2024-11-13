699a31ed453da0d103790cc11a2eb507f4ab80f3
Tafi-Solitude-uit <22521274@gm.uit.edu.vn>
2024-11-13 20:57:27 +0700

    update

--- src/modules/key_pair_module/outputs.tf
+++ src/modules/key_pair_module/outputs.tf
@@ -1,4 +1,4 @@
-output "key_pair_id" {
-  value       = aws_key_pair.anhtaiht_auth.id
-  description = "The Id of the Key Pair"
-}
+# output "key_pair_id" {
+#   value       = aws_key_pair.anhtaiht_auth.id
+#   description = "The Id of the Key Pair"
+# }
